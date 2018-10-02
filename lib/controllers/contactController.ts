2381739fca00603fbdb4c8bd2175532777078912
Kautsar Paramahastha <kautsar@docotel.com>
2018-10-02 17:37:17 +0700

    Add query param on get all contacts

--- lib/controllers/contactController.ts
+++ lib/controllers/contactController.ts
@@ -1,18 +1,25 @@
 import mongoose from "mongoose";
 import { ContactSchema } from "../models/contactModel";
 import { Request, Response } from "express";
+import _ from "lodash";
 
 const Contact = mongoose.model("Contact", ContactSchema);
 
 export class ContactController {
   public get_all(req: Request, res: Response) {
-    Contact.find({}, (err, result) => {
-      if (err) {
-        res.status(400).send(err);
-      }
+    Contact.find(
+      {
+        firstName: { $regex: `.*${req.query.firstName || ""}.*` },
+        lastName: { $regex: `.*${req.query.lastName || ""}.*` }
+      },
+      (err, result) => {
+        if (err) {
+          res.status(400).send(err);
+        }
 
-      res.json(result);
-    });
+        res.json(result);
+      }
+    );
   }
 
   public get_by_id(req: Request, res: Response) {
